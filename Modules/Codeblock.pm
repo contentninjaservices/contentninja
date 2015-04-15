c877ff437aa40a2059f095b23f8ed5a8efa7d39b
contentninjaservices <rpr@9it.de>
2015-04-16 00:19:07 +0200

    debug loadmodules

--- Modules/Codeblock.pm
+++ Modules/Codeblock.pm
@@ -21,7 +21,7 @@ sub run{
 	my ($self,$text) = @_;
 	print "Plugin: Codeblock loaded.\n";
 	# $text =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$self->replacer($1)<\/code><\/pre>/gsx;
-	$val = $text =~ s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/$1/m;
+	$val = $text =~ s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/$1/gsx;
 	printf "Val: %s\n", $val; 
 	$replaced = $self->replacer($val);
 	$text =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$replaced<\/code><\/pre>/gsx;
