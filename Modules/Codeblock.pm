300db0027097ca898c77d12883a472f1ea7db2d4
contentninjaservices <rpr@9it.de>
2015-04-16 01:00:44 +0200

    debug loadmodules

--- Modules/Codeblock.pm
+++ Modules/Codeblock.pm
@@ -15,7 +15,7 @@ sub replacer{
 	my $lt = '<';
 	$val =~ s/\{/$and;/gsx;
 	$val =~ s/\&/$lt;/gsx;
-	# print "Test: $val\n";
+	print "Test: $val\n";
 	return $val
 }
 
@@ -23,13 +23,13 @@ sub run{
 	my ($self,$text) = @_;
 	# print "Plugin: Codeblock loaded.\n";
 	# $txext =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$self->replacer($1)<\/code><\/pre>/gsx;
-  ## $val = $text; 	
-	## $val =~ s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/$1/eg;
+  $val = $text; 	
+	$val =~ s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/$1/eg;
 	## # printf "Val: %s\n Text: %s\n", $val; 
-	## $replaced = $self->replacer($val);
+	$replaced = $self->replacer($val);
 	## # printf "Replaced: %s\n" , $replaced;
-	$text =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$1<\/code><\/pre>/gsm;
-	## $text =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$replaced<\/code><\/pre>/gsx;
+	# $text =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$1<\/code><\/pre>/gsm;
+	$text =~s/\{% codeblock.*?%\}(.*?)\{% endcodeblock %\}/<pre class="code"><code class="code">$replaced<\/code><\/pre>/gsm;
 	## print $text;
 	return $text;
 }
