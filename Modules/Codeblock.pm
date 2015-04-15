67edd3b8a228d86ddb107c13efaccf6364ac15e5
contentninjaservices <rpr@9it.de>
2015-04-16 01:36:30 +0200

    debug loadmodules

--- Modules/Codeblock.pm
+++ Modules/Codeblock.pm
@@ -19,7 +19,7 @@ sub replacer{
 	$val =~ s/\&/$lt/gsx;
 	# print "Test: $val\n";
 	my $enc = encode_base64($val);
-	$return = "<script>var decodedString = Base64.decode($enc);</script>";
+	$return = "<script>var decodedString = Base64.decode($enc); document.write(decodedString); </script>";
 	return $return; 
 }
 
