c869c98652b315d94450c787f3922508f64d920e
contentninjaservices <rpr@9it.de>
2015-04-15 23:51:56 +0200

    debug loadmodules

--- lib/SPP.pm
+++ lib/SPP.pm
@@ -272,7 +272,7 @@ sub loadmodules{
 	foreach (@found) {
   	print "Found: $_\n";
   	$modulname = $_;
-  	eval "use " . $_ . ".pm"";
+  	eval "use " . $_ . ".pm";
   	$mods->{$module} = $modulname->new();
 		# print "Body: XXX" . $body . "XXX\n";
   	$body = $mods->{$module}->run($body);
