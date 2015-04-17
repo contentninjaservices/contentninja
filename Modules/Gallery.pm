34686ee966c578ea4d013c3296136871dc843947
contentninjaservices <rpr@9it.de>
2015-04-17 18:12:41 +0200

    add thumbnail parameter to gallery

--- Modules/Gallery.pm
+++ Modules/Gallery.pm
@@ -3,7 +3,7 @@ package Modules::Gallery;
 #
 # {% gallery %}
 # /gallery/15783866983_27160395b9_b.jpg: Rodeo Dusk (_JonathanMitchellPhotography_)
-# /gallery/10346743894_0cfda8ff7a_b.jpg: Les papillons ont du chagrin (JMS')
+# /gallery/10346743894_0cfda8ff7a_b.jpg|/gallery/MyThumb.jpg: Les papillons ont du chagrin (JMS')
 # /gallery/14395875498_c43e5c4415_b.jpg: Chedder Gorge with goats looking (Si Photography)
 # /gallery/15723733583_b4a7b52459_b.jpg: Pudacuo (OsvinC)
 # {% endgallery %}
@@ -20,13 +20,16 @@ sub run{
 	my $output = "";
 	my $gallery = $text;
 	my (@i,@a) = $gallery =~ /\{% gallery %\}(.*?\.(jpg|gif|jpeg|png)):(.*?)\{% endgallery %\}/m;
-
+	# foreach (@a) { print $_ . "\n"; }
 	my @lines  = split(/\015\012|\012|\015/,$gallery);
 	foreach (@lines) {
 	  my ($file, $ext, $alt) = $_ =~ /(.*?\.(jpg|gif|jpeg|png)): (.*?)$/m;
+		my ($imagefile,$thumbfile) = split(/\|/, $file);
+		# printf "Img: %s, th: %s ext: %s alt: %s\n", $file, $thumbfile, $ext, $alt if ($file ne "" );
 	  my ($thumb) = $file =~ /(.*?)\..*?$/m;
+		if ( $thumbfile eq "" ) { $thumbfile = $thumb."_m.".$ext; }
 		if ( $file ) {
-	  	$output .= sprintf "<a href=\"%s\" class=\"fancybox\" rel=\"group\" title=\"%s\"><img src=\"%s_m.%s\" alt=\"%s\" height=\"200\" /></a>\n", $file, $alt, $thumb, $ext, $alt;
+	  	$output .= sprintf "<a href=\"%s\" class=\"fancybox\" rel=\"group\" title=\"%s\"><img src=\"%s\" alt=\"%s\" height=\"200\" /></a>\n", $imagefile, $alt, $thumbfile, $alt;
 		}
 	}
 	my $styleprefix = "";
